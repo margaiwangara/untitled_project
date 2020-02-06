65684efd6c207c98a115fbd2a8d1349a648cec69
margaiwangara <margaiwangara@gmail.com>
2020-02-06 23:59:04 +0300

    added wakelock package to keep device on while developing

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:wakelock/wakelock.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // Wakelock for development
+    Wakelock.enable();
     return MaterialApp(
       home: Scaffold(
         body: Center(
